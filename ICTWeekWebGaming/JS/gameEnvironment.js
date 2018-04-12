4290469aafa2112d8da0dd160bf720266bec2f6b
mitraillet <steve.henriquet@gmail.com>
2018-04-12 13:24:04 +0100

    dsdq

--- ICTWeekWebGaming/JS/gameEnvironment.js
+++ ICTWeekWebGaming/JS/gameEnvironment.js
@@ -5,14 +5,16 @@ var monsterImg;
 var floorImg;
 var mainCaracImg;
 var myObstacles = [];
+var myBackground;
 var floor = POSTFLOOR - HEIGHTMAINCARAC ;
 var ceil;
 var speedGames = 20 ;
 
 function startGame() {
-    monsterImg = new component(200, 480, "images/scary ghost.gif", 0, 0, "image");
-    floorImg = new component(896, 96, "green", 0, POSTFLOOR);
-    mainCaracImg = new component(HEIGHTMAINCARAC, HEIGHTMAINCARAC, "images/Ghosty ghost.gif", 250, 250, "image");
+    monsterImg = new component(200, 400, "images/scary_ghost2_resized.png", 0, 50, "image");
+    floorImg = new component(896, 96, "brown", 0, POSTFLOOR);
+    myBackground = new background(896, 576, "images/background.jpg", 0, 0, "image");
+    mainCaracImg = new component(HEIGHTMAINCARAC, HEIGHTMAINCARAC, "images/ghosty_ghost1_resized.png", 250, 250, "image");
     myGameArea.start();
 }
 
@@ -44,72 +46,6 @@ var myGameArea = {
     },
     stop : function() {
         clearInterval(this.interval);
-    }
-}
-
-/**
- * Update the game area to make it dynamique
- */
-function updateGameArea() {
-    if (mainCaracImg.crashWith(monsterImg)) {
-        myGameArea.stop();
-    } else {
-        var x, y;
-        var collision = false;
-        var sidecollision = false;
-        for (i = 0; i < myObstacles.length; i += 1) {
-            if(mainCaracImg.sideCollision(myObstacles[i])){
-                sidecollision = true;
-            }
-            if(mainCaracImg.collideWith(myObstacles[i])){
-                floor = mainCaracImg.y;
-                collision = true;
-            }
-            else if (!collision){
-                floor = POSTFLOOR - HEIGHTMAINCARAC;
-            }
-
-        }
-        myGameArea.clear();
-        myGameArea.frameNo += 1;
-        if (myGameArea.frameNo == 1 || everyinterval(150)) {
-            x = myGameArea.canvas.width;
-            minHeight = 20;
-            maxHeight = 200;
-            height = Math.floor(Math.random() * (maxHeight - minHeight + 1) + minHeight);
-            minGap = 50;
-            maxGap = 200;
-            minWidth = 40;
-            maxWidth = 150;
-            width = Math.floor(Math.random() * (maxWidth - minWidth + 1) + minWidth);
-            gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
-            myObstacles.push(new component(width, 50, "green", x, height + gap));
-        }
-        if(everyinterval(2000)){// TODO
-            speedGames--;
-        }
-        for (i = 0; i < myObstacles.length; i += 1) {
-            myObstacles[i].x += -1;
-            myObstacles[i].update();
-        }
-        monsterImg.update();
-        mainCaracImg.x -= 1;
-        mainCaracImg.speedX = 0;
-        mainCaracImg.speedY = 0;
-        if (myGameArea.keys && myGameArea.keys[37]) {
-            mainCaracImg.speedX = -1;
-        }
-        if (myGameArea.keys && myGameArea.keys[39] && sidecollision == false) {
-            mainCaracImg.speedX = 2;
-        }
-        if (myGameArea.keys && myGameArea.keys[32] && mainCaracImg.y == floor) {
-            ceil = floor - 100;
-            mainCaracImg.gravitySpeed = 0;
-            accelerate(-ACELLERATECONST);
-        }
-        mainCaracImg.newPos();
-        mainCaracImg.update();
-        floorImg.update();
     }
 }
 
@@ -214,6 +150,37 @@ function component(width, height, color, x, y, type = "none") {
         return sidecollision;
     }
 }
+
+function background(width, height, color, x, y, type = "none") {
+    this.type = type;
+    if (type == "image") {
+        this.image = new Image();
+        this.image.src = color;
+    }
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.update = function(){
+        ctx = myGameArea.context;
+        if (type == "image") {
+            ctx.drawImage(this.image,
+                this.x,
+                this.y,
+                this.width, this.height);
+        } else {
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+}
+
 function accelerate(n) {
     mainCaracImg.gravity = n;
 }
@@ -221,4 +188,76 @@ function accelerate(n) {
 function everyinterval(n) {
     if ((myGameArea.frameNo / n) % 1 == 0) {return true;}
     return false;
+}
+
+
+/**
+ * Update the game area to make it dynamique
+ */
+function updateGameArea() {
+    if (mainCaracImg.crashWith(monsterImg)) {
+        myGameArea.stop();
+    } else {
+        var x, y;
+        var collision = false;
+        var sidecollision = false;
+        myGameArea.clear();
+        myBackground.newPos();
+        myBackground.update();
+        for (i = 0; i < myObstacles.length; i += 1) {
+            if(mainCaracImg.sideCollision(myObstacles[i])){
+                sidecollision = true;
+            }
+            if(mainCaracImg.collideWith(myObstacles[i])){
+                floor = mainCaracImg.y;
+                collision = true;
+            }
+            else if (!collision){
+                floor = POSTFLOOR - HEIGHTMAINCARAC;
+            }
+        }
+        myGameArea.frameNo += 1;
+        if (myGameArea.frameNo == 1 || everyinterval(150)) {
+            x = myGameArea.canvas.width;
+            minHeight = 20;
+            maxHeight = 200;
+            height = Math.floor(Math.random() * (maxHeight - minHeight + 1) + minHeight);
+            minGap = 50;
+            maxGap = 200;
+            minWidth = 40;
+            maxWidth = 150;
+            width = Math.floor(Math.random() * (maxWidth - minWidth + 1) + minWidth);
+            gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
+            myObstacles.push(new component(width, 50, "images/block.png", x, height + gap, "image"));
+        }
+        if(everyinterval(2000)){// TODO
+            speedGames--;
+        }
+        for (i = 0; i < myObstacles.length; i += 1) {
+            myObstacles[i].x += -1;
+            myObstacles[i].update();
+        }
+        monsterImg.update();
+        mainCaracImg.x -= 1;
+        floorImg.x -= 0.5;
+        mainCaracImg.speedX = 0;
+        mainCaracImg.speedY = 0;
+        if (myGameArea.keys && myGameArea.keys[37]) {
+            mainCaracImg.speedX = -1;
+            mainCaracImg.image.src = "images/ghosty_ghost2_left_resized.png";
+        }
+        if (myGameArea.keys && myGameArea.keys[39] && sidecollision == false) {
+            mainCaracImg.speedX = 2;
+            mainCaracImg.image.src = "images/ghosty_ghost2_resized.png";
+        }
+        if (myGameArea.keys && myGameArea.keys[32] && mainCaracImg.y == floor) {
+            mainCaracImg.image.src = "images/ghosty_ghost2_resized.png";
+            ceil = floor - 100;
+            mainCaracImg.gravitySpeed = 0;
+            accelerate(-ACELLERATECONST);
+        }
+        mainCaracImg.newPos();
+        mainCaracImg.update();
+        floorImg.update();
+    }
 }
